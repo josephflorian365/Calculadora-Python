2d2990600840965021c3a017f85eb8df037633f0
josephflorian365 <jflorianf@vallegrande.edu.pe>
2021-08-30 21:44:59 -0500

    first commit

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+class Calculadora():
+    """docstring for Calculadora."""
+
+    def sumar(self, valor1, valor2):
+        return valor1 + valor2
+
+    def restar(self, valor1, valor2):
+        return valor1 - valor2
+
+    
